51f43758a5ee8994861a88ca7673c7bda1682cf7
Thomas Lhomme <blabla@blabla.com>
2020-06-04 15:03:13 +0200

    ajout de trucs

--- src/main/java/com/myapp/HelloServlet.java
+++ src/main/java/com/myapp/HelloServlet.java
@@ -2,6 +2,7 @@ package com.myapp;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -9,6 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class HelloServlet
  */
+@WebServlet(value="/hello")
 public class HelloServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -23,11 +25,12 @@ public class HelloServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        request.setAttribute("name","Jean-Claude");
+
+        request.getRequestDispatcher("/WEB-INF/pages/showUsers.jsp").forward(request, response);
+    }
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
